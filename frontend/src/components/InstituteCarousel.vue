db9e0f46c10ab941c1fe1d914cc468d5b4e873a8
sancheZ250 <sancheZ2502@yandex.ru>
2023-11-02 23:11:11 +0700

    change style for carousel button

--- frontend/src/components/InstituteCarousel.vue
+++ frontend/src/components/InstituteCarousel.vue
@@ -31,25 +31,53 @@
  })
  </script>
  
- <style>
+ <style scoped>
  .carousel__item {
-    min-height: 200px;
-    width: 100%;
-    height: 600px; /* Добавьте эту строку */
-   object-fit: cover; /* Добавьте эту строку */
-    border-radius: 8px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+   min-height: 200px;
+   width: 100%;
+   max-height: 600px; /* Установите максимальную высоту */
+   max-width: 100%; /* Установите максимальную ширину */
+   object-fit: contain; /* Сохраняет соотношение сторон и уменьшает изображение */
+   border-radius: 8px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
  }
  
  .carousel__slide {
-    padding: 10px;
+   padding: 10px;
  }
  
- .carousel__prev,
+ .carousel-navigation,
+ .carousel-pagination {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+ }
+ 
+ .carousel-navigation button,
+ .carousel-pagination button {
+   background-color: var(--vc-clr-primary);
+   color: var(--vc-clr-white);
+   border: none;
+   border-radius: 100%; /* Создает круглую кнопку */
+   width: 100px;
+   height: 100px;
+   font-size: 20px;
+   margin: 0 5px;
+   cursor: pointer;
+ }
+ 
+ .carousel-navigation button:hover,
+ .carousel-pagination button:hover {
+   background-color: var(--vc-clr-secondary); /* Изменяет цвет при наведении */
+ }
+ 
+ .carousel__prev {
+   margin-right: 10px;
+ }
+ 
  .carousel__next {
-    box-sizing: content-box;
-    border: 5px solid white;
+   margin-left: 10px;
  }
  </style>
