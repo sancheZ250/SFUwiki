e3cedb4dad21d27dee60e61bda3ba7b1afbabd48
NaXy9 <naxy9bl9@gmail.com>
2023-11-04 15:28:08 +0700

    made a home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,26 @@
 <script setup>
+import backgroundImage from '../assets/homesection.jpg';
 
 </script>
 
 <template>
-<h1 class="text-3xl font-bold underline">Home</h1>
+
+<section class="bg-center bg-no-repeat bg-gray-700 bg-blend-multiply" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Добро пожаловать в СФУ вики</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Здесь вы можете узнать про преподавателей немного больше подробностей от прошлых студентов.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <router-link to="/teachers" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Найти препода
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </router-link>
+            <router-link to="/institutes" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Посмотреть институты
+            </router-link>  
+        </div>
+    </div>
+</section>
+
 </template>
