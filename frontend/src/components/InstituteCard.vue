5d1ebc42d3e922c00259f1e99f0806fae3bc1795
NaXy9 <naxy9bl9@gmail.com>
2023-11-05 17:45:01 +0700

    fix InstituteCard and TeacherCard

--- frontend/src/components/InstituteCard.vue
+++ frontend/src/components/InstituteCard.vue
@@ -5,7 +5,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="max-w-sm bg-gray-900 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-sm bg-white dark:bg-gray-900 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <div class="image-container">
           <img class="rounded-t-lg" :src="institute.logo" :alt="institute.abbreviation" />
@@ -13,8 +13,8 @@ defineProps({
       </a>
       <div class="p-5">
         <router-link :to="'/institutes/' + institute.id">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">{{ institute.abbreviation }}</h5>
-        <p class="mb-3 font-normal text-gray-300 dark:text-gray-400">{{ institute.name }}</p>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ institute.abbreviation }}</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ institute.name }}</p>
         <a class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
           Перейти
           <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
